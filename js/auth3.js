b8260baa083a32ce3ee249d272458d45b9c67abc
modales03 <daviddermo56@gmail.com>
2025-04-02 21:54:58 -0500

    auth3.js

--- js/auth3.js
+++ js/auth3.js
@@ -1,7 +1,7 @@
 export const authenticateUser = (email, role, password) => {
     const isValid = email === 'proveedor@gmail.com' && 
                    role === 'proveer' && 
-                   password === '12345678';
+                   password === 'ProvHomeCopy_03';
     return {
       success: isValid,
       message: isValid ? 'Bienvenido de nuevo proveedor!' : 'Credenciales incorrectas'
