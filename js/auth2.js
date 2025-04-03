0b7bb3bcfa3bc70572f2f465680ead4800a82fe1
modales03 <daviddermo56@gmail.com>
2025-04-02 21:54:30 -0500

    auth2.js

--- js/auth2.js
+++ js/auth2.js
@@ -1,7 +1,7 @@
 export const authenticateUser = (email, role, password) => {
     const isValid = email === 'usuario@gmail.com' && 
                    role === 'user' && 
-                   password === '12345678';
+                   password === 'UserHomeCopy_02';
     return {
       success: isValid,
       message: isValid ? 'Bienvenido de nuevo Usuario!' : 'Credenciales incorrectas'
