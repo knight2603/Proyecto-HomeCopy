b8ec8d2297c35865a720758ac888fc4caed54a77
knight2603 <sierrasebastian206@gmail.com>
2025-04-06 11:04:34 -0500

    Delete js/auth.js

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-export const authenticateUser = (email, role, password) => {
-    const isValid = email === 'administrador@gmail.com' && 
-                   role === 'Administrador' && 
-                   password === 'AdminHomeCopy_01';
-    return {
-      success: isValid,
-      message: isValid ? 'Bienvenido de nuevo Administrador!' : 'Credenciales incorrectas'
-    };
-  };
