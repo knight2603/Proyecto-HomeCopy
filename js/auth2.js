dbd2019fc27bad6442532a6bdcac1272a4f6e08f
knight2603 <sierrasebastian206@gmail.com>
2025-04-06 11:04:43 -0500

    Delete js/auth2.js

--- js/auth2.js
+++
@@ -1,9 +0,0 @@
-export const authenticateUser = (email, role, password) => {
-    const isValid = email === 'usuario@gmail.com' && 
-                   role === 'user' && 
-                   password === 'UserHomeCopy_02';
-    return {
-      success: isValid,
-      message: isValid ? 'Bienvenido de nuevo Usuario!' : 'Credenciales incorrectas'
-    };
-  };
