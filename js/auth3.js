3bd9ec11ab64e37021a35e337ecc22aa420b1fb7
knight2603 <sierrasebastian206@gmail.com>
2025-04-06 11:04:57 -0500

    Delete js/auth3.js

--- js/auth3.js
+++
@@ -1,9 +0,0 @@
-export const authenticateUser = (email, role, password) => {
-    const isValid = email === 'proveedor@gmail.com' && 
-                   role === 'proveer' && 
-                   password === 'ProvHomeCopy_03';
-    return {
-      success: isValid,
-      message: isValid ? 'Bienvenido de nuevo proveedor!' : 'Credenciales incorrectas'
-    };
-  };
